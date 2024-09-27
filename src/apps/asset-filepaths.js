c069ce73228e50641d197fd1f067700a36e6129b
TheComamba <98019967+TheComamba@users.noreply.github.com>
2024-09-27 17:31:37 +0200

    Basic UI adjustments (#14)

---
+++ src/apps/asset-filepaths.js
@@ -0,0 +1,19 @@
+class AssetFilepaths extends Application {
+    constructor() {
+        super();
+        this.context = {};
+    }
+
+    get template() {
+        return `modules/asset_auditor/apps/asset-filepaths.hbs`;
+    }
+
+    async getData(options = {}) {
+        this.context = await super.getData(options);
+        this.context.title = game.i18n.localize("asset_auditor.asset-filepaths");
+        this.context.content = "Content";
+        return this.context;
+    }
+}
+
+export { AssetFilepaths };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
+import { AssetFilepaths } from "../apps/asset-filepaths.js";
 
+Hooks.on("renderSidebarTab", async (app, html) => {
+    if (app instanceof Settings) {
+        let button_text = game.i18n.localize("asset_auditor.asset-filepaths");
+        let button = $(`<button class='asset-list'><i class='fas fa-file-edit'></i> ${button_text}</button>`)
+
+        button.click(function () {
+            new AssetFilepaths().render(true);
+        });
+
+        let settings_sidebar = html.find("div#settings-game");
+        if (settings_sidebar.length == 0) {
+            ui.notifications.error(game.i18n.localize("asset_auditor.sidebar-not-found"));
+            return;
+        }
+        settings_sidebar.append(button);
+    }
+})
