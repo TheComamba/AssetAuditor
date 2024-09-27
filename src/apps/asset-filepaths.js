3aa5b0ebdc456af0163b736e1dc4c61ba5696ad6
TheComamba <thecomamba@jojoheinze.de>
2024-09-27 17:30:35 +0200

    Making handlebar more foundry-esque. I think.

--- src/apps/asset-filepaths.js
+++ src/apps/asset-filepaths.js
@@ -11,9 +11,7 @@ class AssetFilepaths extends Application {
     async getData(options = {}) {
         this.context = await super.getData(options);
         this.context.title = game.i18n.localize("asset_auditor.asset-filepaths");
-        this.context.header = game.i18n.localize("asset_auditor.asset-filepaths");
         this.context.content = "Content";
-        this.context.footer = "Footer";
         return this.context;
     }
 }
