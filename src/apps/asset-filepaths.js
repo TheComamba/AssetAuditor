307d6bdd84601016de471fbae676e4c4eae3ccf3
TheComamba <thecomamba@jojoheinze.de>
2024-10-07 16:38:28 +0200

    Reset button for path input fields.

--- src/apps/asset-filepaths.js
+++ src/apps/asset-filepaths.js
@@ -58,13 +58,30 @@ class AssetFilepaths extends Application {
         });
 
         html.find('.update-button').click((event) => {
-            const button = $(event.currentTarget);
-            const input = button.siblings('.path-input');
+            const updateButton = $(event.currentTarget);
+            const resetButton = updateButton.siblings('.reset-button');
+            const input = updateButton.siblings('.path-input');
             const inputValue = input.val();
-            const assetId = button.data('asset-id');
+            const assetId = updateButton.data('asset-id');
             this.updateAssetPath(assetId, inputValue);
             input.data('original-value', inputValue);
-            button.css('opacity', 0);
+            resetButton.css('opacity', 0);
+            resetButton.prop('disabled', true);
+            updateButton.css('opacity', 0);
+            updateButton.prop('disabled', true);
+            this.refresh(html);
+        });
+
+        html.find('.reset-button').click((event) => {
+            const resetButton = $(event.currentTarget);
+            const updateButton = resetButton.siblings('.update-button');
+            const input = resetButton.siblings('.path-input');
+            const originalValue = input.data('original-value');
+            input.val(originalValue);
+            resetButton.css('opacity', 0);
+            resetButton.prop('disabled', true);
+            updateButton.css('opacity', 0);
+            updateButton.prop('disabled', true);
             this.refresh(html);
         });
 
@@ -97,11 +114,16 @@ class AssetFilepaths extends Application {
             const input = $(event.currentTarget);
             const originalValue = input.data('original-value');
             const currentValue = input.val();
+            const resetButton = input.siblings('.reset-button');
             const updateButton = input.siblings('.update-button');
             if (currentValue !== originalValue) {
+                resetButton.css('opacity', 1);
+                resetButton.prop('disabled', false);
                 updateButton.css('opacity', 1);
                 updateButton.prop('disabled', false);
             } else {
+                resetButton.css('opacity', 0);
+                resetButton.prop('disabled', true);
                 updateButton.css('opacity', 0);
                 updateButton.prop('disabled', true);
             }
